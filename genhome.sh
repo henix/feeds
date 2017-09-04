d06ed48672567d0aa451c4747eca9276731a71df
henix <shellpick@gmail.com>
2017-09-04 17:45:01 +0800

    将首页按照分类排列

--- genhome.sh
+++ genhome.sh
@@ -4,6 +4,7 @@ cat <<EOF
 <html>
 <head>
 <meta charset="utf-8">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" />
 </head>
 <body>
 <ul>
@@ -14,6 +15,9 @@ for d in $(find . -maxdepth 1 -type d | sed -e 's|^./||' -e '/^\.\|\.git$/d' | s
 done
 cat <<EOF
 </ul>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ramda/0.24.1/ramda.min.js"></script>
+<script src="https://cdn.rawgit.com/jed/domo/13c45aba3e94dd2d1bc469ce3339bbc1e3a10314/lib/domo.js"></script>
+<script src="index.js"></script>
 </body>
 </html>
 EOF
---
+++ index.js
@@ -0,0 +1,50 @@
+const feeds = [
+	["Finance", [
+		"chinaetfs",
+		"SmartGAA",
+		"touyanbang88",
+		"forex-analysis",
+		"Chouma2016",
+		"GlobalMacroHedge",
+		"sishimiao",
+		"DimensionsCapital",
+	]],
+	["独立", [
+		"wxieshuo",
+		"aloha-yaoyao",
+		"gh_7d1aa4ada621",
+		"iamawguo",
+	]],
+	["FP", [
+		"yinwang",
+		"impress-your-cat",
+		"marisa",
+		"rust-lang",
+	]],
+	["Security", [
+		"drops",
+	]],
+	["政治 / 人文", [
+		"knowledgewealth",
+		"mzmojo",
+		"zhidaoxue",
+		"zhidaoshe",
+	]],
+	["商业", [
+		"caozsay",
+		"Professor-Li",
+	]],
+	["心理", [
+		"PSY_Jile",
+	]],
+	["科学", [
+		"liweitan2014",
+	]],
+];
+
+const lis = R.fromPairs([].slice.call(document.getElementsByTagName("li")).map(li => [/(?:^|\.)([^.]+)\/$/.exec(li.getElementsByTagName("a")[0].getAttribute("href"))[1], li]))
+
+document.body.appendChild(DIV.apply(null, R.flatten(feeds.map(cate => [
+	H2(cate[0]),
+	UL.apply(null, cate[1].map(id => lis[id])),
+]))));
