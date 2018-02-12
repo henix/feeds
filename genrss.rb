7ce995c1cb8a1d286775ed7e1801362fb0a68bf9
henix <shellpick@gmail.com>
2018-02-12 16:25:10 +0800

    修正rss内容

--- genrss.rb
+++ genrss.rb
@@ -36,13 +36,12 @@ buf = %{<?xml version="1.0" encoding="utf-8"?>
 } +
 	toc.reverse.take(10).map { |t|
 		guid = t[:date] + "-" + t[:id]
-		filename = guid + ".html"
 		orightml = %{<p><a href="#{e(t[:link])}">原文</a></p>
 }
 		%{<item>
 <title>#{e(t[:title])}</title>
-<link>#{e(sitelink + l(filename))}</link>
-<description>#{e(orightml + IO.read(filename))}</description>
+<link>#{e(sitelink + l(guid + ".html"))}</link>
+<description>#{e(orightml + IO.read(guid + ".htm"))}</description>
 <author>#{e(t[:author])}</author>
 <guid isPermaLink="false">#{e(guid)}</guid>
 <pubDate>#{e(Time.parse(t[:date]).rfc822)}</pubDate>
