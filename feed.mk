223216a7c087225ed5c247a07b52d6d290ddc4aa
henix <shellpick@gmail.com>
2019-03-27 21:36:26 +0800

    添加百度统计

--- feed.mk
+++ feed.mk
@@ -9,13 +9,13 @@ all: index.html index.xml $(HTMLS)
 index.toc: $(TOCS)
 	cat $^ | sort -t '	' -k 1,2 > $@
 
-index.html: ../genindex.rb index.meta index.toc
+index.html: ../genindex.rb index.meta index.toc ../ga.seg.htm
 	../updated.sh $@ $^
 
 index.xml: ../genrss.rb index.meta index.toc $(HTMS)
 	../genrss.rb "https://henix.github.io/feeds/$$(pwd | tr '/' '\n' | tail -n1)" > $@
 
-%.html: ../page.rb %.htm %.toc
+%.html: ../page.rb %.htm %.toc ../ga.seg.htm
 	$^ > $@
 
 clean:
